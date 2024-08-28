2cbcc6ee693e3dfe06e1fedad7e22ee9239e052f
Gabrielle Pagnard <gpagnard@gmail.com>
2024-08-28 06:01:44 +0200

    Ajout documentation technique

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,26 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';
 import App from './App.jsx';
-import './index.css';
+import './index.css'; // Importation des styles globaux
 
-// Sélectionnez l'élément root
+/**
+ * Sélectionne l'élément HTML avec l'ID 'root'.
+ * Cet élément sert de conteneur principal pour l'application React.
+ * @type {HTMLElement}
+ */
 const rootElement = document.getElementById('root');
 
-// Utilisez createRoot pour React 18
+/**
+ * Initialise la racine de l'application en utilisant createRoot, 
+ * qui est recommandé pour les applications React 18 et offre de meilleures performances et compatibilité.
+ * @type {Root}
+ */
 const root = createRoot(rootElement);
 
+/**
+ * Rendu de l'application principale dans le conteneur root.
+ * ChakraProvider est utilisé pour gérer les styles et les thèmes de l'application.
+ */
 root.render(
   <ChakraProvider>
     <App />
